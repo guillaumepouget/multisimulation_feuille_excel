52e029c852bbd03b527748b47e0d735648a256d5
Guillaume Pouget <80262789+guillaumepouget@users.noreply.github.com>
2022-01-31 15:10:07 +0100

    Update script_cluster.sh

--- script_cluster.sh
+++ script_cluster.sh
@@ -11,6 +11,6 @@ stics_version=\"${stics_version}\"
 javastics_path=\"${javastics_path}\"
 option_effacement_fichiers=\"${option_effacement_fichiers}\"
 #
-RES1=$(sbatch --parsable --nodes=1 --cpus-per-task=1 jobSLURM_1.sh $rep_multisimu $stics_version $javastics_path)
+RES1=$(sbatch --parsable --nodes=1 --cpus-per-task=1 jobSLURM_1.sh $rep_multisimu $stics_version $javastics_path $option_effacement_fichiers)
 RES2=$(sbatch --parsable --dependency=afterok:$RES1 --array=1-$ncoeurs --nodes=1 --cpus-per-task=1 jobSLURM_2.sh $rep_multisimu $javastics_path $var_names $ncoeurs)
-sbatch --dependency=afterany:$RES2 --nodes=1 --cpus-per-task=1 jobSLURM_3.sh $rep_multisimu $ncoeurs
+sbatch --dependency=afterany:$RES2 --nodes=1 --cpus-per-task=1 jobSLURM_3.sh $rep_multisimu $ncoeurs $option_effacement_fichiers
